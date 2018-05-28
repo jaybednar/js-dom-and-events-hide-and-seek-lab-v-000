42ef1b5b45b118bf6ec2579e2566f9e1f287bd13
Jay Bednar <ironviralmedia@gmail.com>
2018-05-28 16:06:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+	document.getElementByTagName(selector);
+}
+
+function nestedTarget() {
+	document.querySelector('div#nested div.target');
+}
+
+function increaseRankBy(n) {
+	const list = document.querySelectorAll('ul.ranked-list li')
+
+	for (let i = 0; i < list.length; i++) {
+		list[i].innerHTML = (parseInt(list[i].innerHTML) + 5).toString();
+	}
+}
+
+function deepestChild() {
+	const deepest = document.querySelector('div#grand-node div div div div')
+	return deepest
+}
